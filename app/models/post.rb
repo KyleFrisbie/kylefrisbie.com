13bbe718346023d5ffee0870a019bc4b539a34d3
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-03-14 23:00:22 -0600

    config disqus gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :tags
+  acts_as_disqusable
 end
--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,6 +1,6 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "kylefrisbie.com"
-  config::SECRET_KEY =
-  config::PUBLIC_KEY =
-  config::ACCESS_TOKEN = "your_access_token" #you got it, right? ;-)
+  config::SHORT_NAME = "kylefrisbie"
+  config::SECRET_KEY ="GPhx6JGL8QDsZrBQjHQldHH48zkJKJN5lE5UB8JQZ7ZZJC271oX0wvM41ZLmrifk"
+  config::PUBLIC_KEY = "DLBCCkhSwbrOY6qLZzYWCgDtyp2aDsUlzUi6Psm4YaUX3ZLFixuclWXq1zvLTHcv"
+  config::ACCESS_TOKEN = "63e928d6eb764c939f1c650f8e897491" #you got it, right? ;-)
 end
